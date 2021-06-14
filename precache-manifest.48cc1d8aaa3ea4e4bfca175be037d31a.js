6fecf3c6cb40cae590449aa8f5e0bdc7481a5d75
saulokirchmaier <saulokirchmaier@gmail.com>
2021-06-14 10:46:54 -0300

    Updates

---
+++ precache-manifest.48cc1d8aaa3ea4e4bfca175be037d31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f260f013cd5e65542a55d7d8960fea",
+    "url": "/no-master-store-shopping/index.html"
+  },
+  {
+    "revision": "8d5dfae882c9bbc2b8e9",
+    "url": "/no-master-store-shopping/static/css/main.8eb3a17e.chunk.css"
+  },
+  {
+    "revision": "85fd6477c5541e3b5c11",
+    "url": "/no-master-store-shopping/static/js/2.da1a35fe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/no-master-store-shopping/static/js/2.da1a35fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5dfae882c9bbc2b8e9",
+    "url": "/no-master-store-shopping/static/js/main.00fa163f.chunk.js"
+  },
+  {
+    "revision": "102450619418952d3b0c",
+    "url": "/no-master-store-shopping/static/js/runtime-main.a2f9e188.js"
+  }
+]);
